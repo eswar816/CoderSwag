5d9bf47b1e20ab0f0ae8501e5e89d0c2b2653f1b
eswar <sainath214224@gmail.com>
2020-07-13 16:57:01 +0530

    lesson-04

--- app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -11,12 +11,12 @@ import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import kotlinx.android.synthetic.main.category_list_item.view.*
 
-class CategoryRecycleAdapter(val context: Context,val categories:List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+class CategoryRecycleAdapter(val context: Context,val categories:List<Category>,val itemClick:(Category)->Unit) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view=LayoutInflater.from(context)
             .inflate(R.layout.category_list_item,parent,false)
-        return  Holder(view)
+        return  Holder(view,itemClick)
     }
 
     override fun getItemCount(): Int {
@@ -24,10 +24,10 @@ class CategoryRecycleAdapter(val context: Context,val categories:List<Category>)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bindCategory(categories[position],context)
+        holder?.bindCategory(categories[position],context)
     }
 
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View,val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
         val categoryImage=itemView.findViewById<ImageView>(R.id.categoryImage)
         val categoryName=itemView?.findViewById<TextView>(R.id.categoryName)
         fun bindCategory(category: Category,context: Context){
@@ -35,6 +35,7 @@ class CategoryRecycleAdapter(val context: Context,val categories:List<Category>)
             "drawable",context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text=category.title
+            itemView.setOnClickListener { itemClick(category) }
         }
     }
 
